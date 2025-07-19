ece2b0c7093e22395597f80f20ced41a55dfb526
RomFur <129603951+RomFur@users.noreply.github.com>
2025-07-19 19:40:29 +0200

    Create createDb.sql
    Query to create the MySQL database before using app

---
+++ createDb.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS articles_db;
+USE articles_db;
+
+CREATE TABLE IF NOT EXISTS articles (
+	ut VARCHAR(50) PRIMARY KEY,
+    title TEXT NOT NULL,
+    year INT,
+    sport VARCHAR(100),
+    population TEXT,
+    technology TEXT,
+    outcome TEXT
+);
